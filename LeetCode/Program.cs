90ddab11335e883f531bb31501493690a57ce9dd
Vasily Kuznetsov <31756050+RunoLight@users.noreply.github.com>
2023-08-30 13:09:07 +1000

    Initial

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace LeetCode;
+
+public abstract class Program
+{
+    public static void Main(string[] args)
+    {
+    }
+}
