12b0f41e016c10996ad77403114950fd2130bc8f
Vasily Kuznetsov <31756050+RunoLight@users.noreply.github.com>
2023-09-04 16:21:13 +1000

    Ozon #3

---
+++ LeetCode/Ozon Route 256 Middle Contest 2023/F.cs
@@ -0,0 +1,223 @@
+﻿// F. JSON категории (25 баллов)
+// На маркетплейсе у каждого товара есть категория. При этом, у некоторых категорий есть дочерние
+// категории. Для удобной навигации по маркетплейсу покупатели могут пользоваться деревом категорий.
+//
+// Ваша задача — построить дерево категорий. Дана информация об отношениях родительских и дочерних
+// категорий в виде JSON-массива. Каждый элемент массива является словарем, с полями name (название
+// категории), id (числовой идентификатор категории) и parent (числовой идентификатор родительской
+// категории). Известно, что корневая категория имеет нулевой идентификатор и не имеет идентификатора
+// родительской категории.
+// По данной информации постройте дерево категорий в виде JSON-словаря. Словарь для каждой категории
+// должен иметь поля name, id и массив next, состоящий из таких же словарей для дочерних категорий.
+//
+// Входные данные
+// Каждый тест состоит из нескольких наборов входных данных. Первая строка содержит целое число t
+// (1≤t≤100) — количество наборов входных данных. Далее следует описание наборов входных данных.
+//
+// Первая строка каждого набора входных данных содержит целое число n (1≤n≤1000) — количество строк
+// с описанием JSON-массива категорий.
+//
+// Следующие n строк содержат описание JSON-массива категорий. Все числовые идентификаторы категорий
+// являются целыми числами и удовлетворяют условию 0≤id≤10^9. Все имена категорий непустые, состоят
+// из строчных латинских букв и имеют длину не больше 20. В описании могут быть символы пробела и табуляции.
+//
+// Гарантируется, что каждый набор входных данных содержит корневую категорию и не более 400 категорий.
+// Гарантируется, что размер входных данных не превосходит 10Мб.
+//
+// Выходные данные
+// Выведите JSON-массив из t элементов. i-й элемент массива является словарем с описанием дерева
+// категорий для i-го набора входных данных. При проверке ответа пробелы, табы и переносы строки не
+// учитываются(кроме таковых в json полях). Порядок полей в словаре и порядок дочерних категорий в
+// массиве next не учитывается. Если у категории нет дочерних категорий, ключ next может отсутствовать,
+// или соответствовать пустому массиву.
+//
+// Тесты:
+//
+// Тест №1
+// Входные данные:
+// 2
+// 21
+// [
+//    {
+//      "id":0,
+//      "name":"all"
+//    },
+//    {
+//       "id":1,
+//       "name":"clothes",
+//       "parent":0
+//    },
+//    {
+//       "id":2,
+//       "name":"shoes",
+//       "parent":0
+//    },
+//    {
+//       "id":55,
+//       "name":"sneakers",
+//       "parent":2
+//    }
+// ]
+// 6
+//  [ {"parent":	0,"id":100,  "name":
+//  "x"},{
+//
+// "name":"x","id":0}
+//
+// ]
+//
+// Выходные данные:
+// [{
+// 	"id": 0,
+// 	"name": "all",
+// 	"next": [{
+// 		"id": 1,
+// 		"name": "clothes",
+// 		"next": []
+// 	}, {
+// 		"id": 2,
+// 		"name": "shoes",
+// 		"next": [{
+// 			"id": 55,
+// 			"name": "sneakers"
+// 		}]
+// 	}]
+// },
+// {"name":"x","id":0,"next":[{"id":100,"name":"x"}]}
+// ]
+//
+// Тест №2
+// Входные данные:
+// 1
+// 9
+// [
+//  {"name": "everything", "id": 0},
+//  {"name": "clothes", "id": 1, "parent": 0},
+//  {"name": "electronics", "id": 2, "parent": 0},
+//  {"name": "computers", "id": 4, "parent": 2},
+//  {"name": "aio", "id": 3, "parent": 4},
+//  {"name": "tv", "id": 5, "parent": 2},
+//  {"name": "house", "id": 6, "parent": 0}
+// ]
+// Выходные данные:
+// [
+// {"id":0,"name":"everything","next":[{"id":1,"name":"clothes","next":[]},{"id":2,"name":"electronics","next":[{"id":4,"name":"computers","next":[{"id":3,"name":"aio","next":[]}]},{"id":5,"name":"tv","next":[]}]},{"id":6,"name":"house","next":[]}]}
+// ]
+
+namespace LeetCode.Ozon_Route_256_Middle_Contest_2023;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public static class TaskF
+{
+    public static void F()
+    {
+        List<CategoryTree> answers = new List<CategoryTree>();
+        int t = int.Parse(Console.ReadLine()!);
+
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine()!);
+            StringBuilder inputDataBuilder = new StringBuilder();
+
+            for (int j = 0; j < n; j++)
+            {
+                inputDataBuilder.Append(Console.ReadLine());
+            }
+
+            Category[] categories = JsonSerializer.Deserialize<Category[]>(inputDataBuilder.ToString())!;
+
+            Dictionary<int, List<Category>> parentToChild = new Dictionary<int, List<Category>>();
+            foreach (var category in categories)
+            {
+                if (category.Parent == null)
+                    continue;
+
+                if (!parentToChild.ContainsKey(category.Parent.Value))
+                {
+                    parentToChild[category.Parent.Value] = new List<Category>();
+                }
+
+                parentToChild[category.Parent.Value].Add(category);
+            }
+
+            Category rootCategory = FindRootCategory(categories);
+            CategoryTree tree = BuildCategoryTree(rootCategory, parentToChild);
+
+            answers.Add(tree);
+        }
+
+        Console.Write(JsonSerializer.Serialize(answers, new JsonSerializerOptions
+        {
+            WriteIndented = false,
+            MaxDepth = 10000
+        }));
+    }
+
+    private static Category FindRootCategory(Category[] categories)
+    {
+        foreach (var category in categories)
+        {
+            if (category.Parent == null)
+            {
+                return category;
+            }
+        }
+
+        return null;
+    }
+
+    private static CategoryTree BuildCategoryTree(
+        Category rootCategory, IReadOnlyDictionary<int, List<Category>> parentToChild
+    )
+    {
+        CategoryTree tree = new CategoryTree
+        {
+            Id = rootCategory.Id,
+            Name = rootCategory.Name,
+            Next = new List<CategoryTree>()
+        };
+
+        Stack<(CategoryTree, int)> stack = new Stack<(CategoryTree, int)>();
+        stack.Push((tree, rootCategory.Id));
+
+        while (stack.Count > 0)
+        {
+            var (currentTree, currentCategoryId) = stack.Pop();
+            if (parentToChild.ContainsKey(currentCategoryId))
+            {
+                foreach (var childCategory in parentToChild[currentCategoryId])
+                {
+                    CategoryTree childTree = new CategoryTree
+                    {
+                        Id = childCategory.Id,
+                        Name = childCategory.Name,
+                        Next = new List<CategoryTree>()
+                    };
+                    currentTree.Next.Add(childTree);
+                    stack.Push((childTree, childCategory.Id));
+                }
+            }
+        }
+
+        return tree;
+    }
+}
+
+class Category
+{
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("parent")] public int? Parent { get; set; }
+}
+
+class CategoryTree
+{
+    [JsonPropertyName("id")] public int Id { get; set; }
+    [JsonPropertyName("name")] public string Name { get; set; }
+    [JsonPropertyName("next")] public List<CategoryTree> Next { get; set; }
+}
---
+++ LeetCode/Ozon Route 256 Middle Contest 2023/G.cs
@@ -0,0 +1,256 @@
+﻿// Колода состоит из 52 карт. Каждая карта обозначается одним из тринадцати значений (2, 3, 4, 5, 6, 7, 8, 9, Ten, Jack, Queen, King, Ace) и одной из четырех мастей (Spades, Clubs, Diamonds, Hearts).
+//
+// Выдуманная игра 3-Покер происходит следующим образом.
+//
+// 1) Изначально все n игроков получают по две карты из колоды.
+// 2) После этого на стол выкладывается одна карта из той же колоды.
+// 3) Выигрывают те игроки, у которых собралась самая старшая комбинация.
+//     
+// Для определения самой старшей комбинации, которая собралась у i -го игрока, используются следующие правила:
+// * если две карты у игрока в руке и карта на столе имеют одинаковое значение, игрок собрал комбинацию 'Сет со значением x';
+// * если из двух карт у игрока в руке и карты на столе можно выбрать две карты с одинаковым значением x, игрок собрал комбинацию 'Пара со значением x';
+// * иначе, берется карта с самым старшим значением из двух карт у игрока в руке и карты на столе, тогда игрок собрал комбинацию 'Старшая карта x'.
+// Любой сет старше пары, а любая пара старше комбинации старшая карта. Из одинаковых комбинаций старше та, у которой старше значение. Если одинаковая самая старшая комбинация есть у нескольких игроков, все они объявляются выигрывшими.
+//
+// Вы — первый игрок. Вам известно, какие карты получил на руки каждый игрок. Определите, какую карту можно выложить на стол, чтобы вы оказались в числе победителей.
+//
+// Входные данные
+// Каждый тест состоит из нескольких наборов входных данных. Первая строка содержит целое число t(1≤t≤10^3) — количество наборов входных данных. Далее следует описание наборов входных данных.
+//
+// Первая строка каждого набора входных данных содержит целое число n(2≤n≤25) — количество игроков.
+//
+// Следующие n строк каждого набора входных данных содержат описания двух карт, разделенных пробелом — карты, которые получил на руки i -й игрок.
+//
+// Описание карты состоит из двух символов, записанных подряд: значения и масти.
+//
+// Выходные данные Для каждого набора входных данных выведите в первой строке количество карт k, которые можно выложить на стол для вашей победы. В следующих k строках выведите описания этих карт. Выводить описания можно в любом порядке.
+//  
+// Тест №1:
+// Входные данные:
+// 4
+// 2
+// TS TC
+// AD AH
+// 3
+// 2H 3H
+// 9S 9C
+// 4D QS
+// 3
+// 4C 7H
+// 4H 4D
+// 6S 6H
+// 3
+// 2S 3H
+// 2C 2D
+// 3C 3D
+//
+// Выходные данные:
+// 2
+// TD
+// TH
+// 0
+// 3
+// 7S
+// 7C
+// 7D
+// 0
+//
+// Тест №2:
+// Входные данные:
+// 1
+// 7
+// AS AC
+// AD AH
+// KS JH
+// 9D 9C
+// 5H 5D
+// 3C 3S
+// TC TH
+//
+// Выходные данные:
+// 30
+// 2S
+// 2C
+// 2D
+// 2H
+// 4S
+// 4C
+// 4D
+// 4H
+// 6S
+// 6C
+// 6D
+// 6H
+// 7S
+// 7C
+// 7D
+// 7H
+// 8S
+// 8C
+// 8D
+// 8H
+// JS
+// JC
+// JD
+// QS
+// QC
+// QD
+// QH
+// KC
+// KD
+// KH
+
+namespace LeetCode.Ozon_Route_256_Middle_Contest_2023;
+
+using System.Text;
+
+public static class TaskG
+{
+    public static void G()
+    {
+        int t = int.Parse(Console.ReadLine()!);
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < t; i++)
+        {
+            Poker(answer);
+        }
+
+        Console.Write(answer.ToString().Trim());
+    }
+
+    private static void Poker(StringBuilder answer)
+    {
+        int players = int.Parse(Console.ReadLine()!);
+        List<Card> deck = (from value in Card.Values from suit in Card.Suits select new Card(value, suit)).ToList();
+
+        Player me = new Player(new Card("A", "S"), new Card("A", "S")); // Preventing possible un-initialed
+        List<Player> otherPlayers = new List<Player>();
+
+        for (int j = 0; j < players; j++)
+        {
+            string[] cardInfo = Console.ReadLine()!.Split();
+            Card card1 = deck.First(card =>
+                card.Value.Equals(cardInfo[0][0].ToString()) && card.Suit.Equals(cardInfo[0][1].ToString()));
+            Card card2 = deck.First(card =>
+                card.Value.Equals(cardInfo[1][0].ToString()) && card.Suit.Equals(cardInfo[1][1].ToString()));
+
+            deck.Remove(card1);
+            deck.Remove(card2);
+
+            if (j == 0)
+            {
+                me = new Player(card1, card2);
+            }
+            else
+            {
+                otherPlayers.Add(new Player(card1, card2));
+            }
+        }
+
+        List<Card> winningCards = new List<Card>();
+        foreach (Card card in deck)
+        {
+            (CombinationType type, int value) = me.GetCombination(card);
+            bool otherPlayerWon = false;
+
+            foreach (Player otherPlayer in otherPlayers)
+            {
+                (CombinationType otherType, int otherValue) = otherPlayer.GetCombination(card);
+                if (otherType > type || (otherType == type && otherValue > value))
+                {
+                    otherPlayerWon = true;
+                    break;
+                }
+            }
+
+            if (!otherPlayerWon)
+            {
+                winningCards.Add(card);
+            }
+        }
+
+        answer.AppendLine(winningCards.Count.ToString());
+        foreach (var card in winningCards)
+            answer.AppendLine($"{card.Value}{card.Suit}");
+    }
+}
+
+internal class Player
+{
+    public Player(Card first, Card second)
+    {
+        _cards = new Card[2];
+        _cards[0] = first;
+        _cards[1] = second;
+    }
+
+    private readonly Card[] _cards;
+
+    public (CombinationType type, int valueIndex) GetCombination(Card card)
+    {
+        CombinationType type;
+        int valueIndex;
+
+        var a = _cards.Append(card).ToList();
+        if (a[0].Value.Equals(a[1].Value) && a[1].Value.Equals(a[2].Value))
+        {
+            type = CombinationType.Triple;
+            valueIndex = Card.CardsList.IndexOf(card.Value);
+        }
+        else if (a[0].Value.Equals(a[1].Value))
+        {
+            type = CombinationType.Double;
+            valueIndex = Card.CardsList.IndexOf(a[0].Value);
+        }
+        else if (a[1].Value.Equals(a[2].Value))
+        {
+            type = CombinationType.Double;
+            valueIndex = Card.CardsList.IndexOf(a[1].Value);
+        }
+        else if (a[0].Value.Equals(a[2].Value))
+        {
+            type = CombinationType.Double;
+            valueIndex = Card.CardsList.IndexOf(a[2].Value);
+        }
+        else
+        {
+            type = CombinationType.One;
+            valueIndex = new List<int>
+            {
+                Card.CardsList.IndexOf(a[0].Value),
+                Card.CardsList.IndexOf(a[1].Value),
+                Card.CardsList.IndexOf(a[2].Value)
+            }.Max();
+        }
+
+        return (type, valueIndex);
+    }
+}
+
+enum CombinationType
+{
+    Triple = 3,
+    Double = 2,
+    One = 1
+}
+
+class Card
+{
+    public static readonly List<string> CardsList;
+    public static readonly string[] Values = { "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A" };
+    public static readonly string[] Suits = { "S", "C", "D", "H" };
+
+    public string Value { get; }
+    public string Suit { get; }
+
+    static Card()
+    {
+        CardsList = Values.ToList();
+    }
+
+    public Card(string value, string suit)
+    {
+        Value = value;
+        Suit = suit;
+    }
+}
